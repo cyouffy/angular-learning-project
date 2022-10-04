eebe0884cad24ef2ec513e56f74e0ad85a84df57
cyouffy <cyouffy@gmail.com>
2022-10-04 08:37:09 +0300

    Adding directive highlighted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,32 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { Course } from './model/course';
+import {Course} from './model/course';
+import {CourseCardComponent} from './course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
 
-  courses = COURSES;
+    courses = COURSES;
 
-  @ViewChild(CourseCardComponent)
-  card: CourseCardComponent;
 
-  @ViewChild('container')
-  container: ElementRef;
+    @ViewChildren(CourseCardComponent, {read: ElementRef})
+    cards : QueryList<ElementRef>;
 
-  OnCourseSelected(course: Course) {
-    console.log(this.container);
-  }
+
+    constructor() {
+
+    }
+
+    ngAfterViewInit() {
+
+    }
+
+    onCourseSelected(course:Course) {
+
+    }
 
 }
