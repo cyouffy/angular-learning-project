83042ac8bd113dc3df46e16eba951101cb544401
cyouffy <cyouffy@gmail.com>
2022-10-04 09:00:40 +0300

    Adding HostBinding to highlighted directive

--- src/app/directives/highlighted.directive.ts
+++ src/app/directives/highlighted.directive.ts
@@ -1,12 +1,20 @@
-import { Directive } from '@angular/core';
+import {Directive, HostBinding, Input} from '@angular/core';
 
 @Directive({
   selector: '[highlighted]'
 })
 export class HighlightedDirective {
 
+  @Input('highlighted')
+  isHighlighted = false;
+
   constructor() {
     console.log("Directive created...");
+  }
+
+  @HostBinding('class.highlighted')
+  get cssClasses() {
+    return this.isHighlighted;
   }
 
 }
