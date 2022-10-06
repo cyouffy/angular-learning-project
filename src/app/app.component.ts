6c82adc82cafdd12dd1dfad94b146d3a8b650448
cyouffy <cyouffy@gmail.com>
2022-10-06 17:57:27 +0300

    Injection token.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,30 @@ import {HttpClient, HttpParams} from '@angular/common/http';
 import * as http from 'http';
 import {logging} from 'protractor';
 import {CoursesService} from './services/courses.service';
+import {APP_CONFIG, AppConfig, CONFIG_TOKEN} from './config';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {provide: CONFIG_TOKEN,
+    useFactory: () => APP_CONFIG}
+  ]
 })
 export class AppComponent implements OnInit {
 
   courses$: Observable<Course[]>;
 
-  constructor(private coursesService: CoursesService) {
+  constructor(private coursesService: CoursesService,
+              @Inject(CONFIG_TOKEN)private conf: AppConfig) {
 
   }
 
   ngOnInit() {
     this.courses$ = this.coursesService.loadCourses();
+
+    console.log(this.conf);
   }
 
   save(course: Course) {
