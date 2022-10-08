42b360bd601f7cb0796f231c2981f24115002e89
cyouffy <cyouffy@gmail.com>
2022-10-08 09:17:28 +0300

    OnPush change detection with observable stream testing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,19 @@
-import {AfterViewInit, Component, ElementRef, Inject, InjectionToken, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Inject, OnInit} from '@angular/core';
 import {COURSES} from '../db-data';
 import {Course} from './model/course';
-import {CourseCardComponent} from './course-card/course-card.component';
-import {HighlightedDirective} from './directives/highlighted.directive';
-import {Observable} from 'rxjs';
-import {HttpClient, HttpParams} from '@angular/common/http';
-import * as http from 'http';
-import {logging} from 'protractor';
 import {CoursesService} from './services/courses.service';
-import {APP_CONFIG, AppConfig, CONFIG_TOKEN} from './config';
+import {AppConfig, CONFIG_TOKEN} from './config';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent implements OnInit {
 
-  courses = COURSES;
+  courses$: Observable<Course[]>;
 
   constructor(private coursesService: CoursesService,
               @Inject(CONFIG_TOKEN)private conf: AppConfig) {
@@ -25,13 +21,10 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.courses$ = this.coursesService.loadCourses();
   }
 
   editCourse() {
-    const course = this.courses[0];
-    const newCourse = {...course};
-    newCourse.description = 'New Value!'
-    this.courses[0] = newCourse;
   }
 
   save(course: Course) {
